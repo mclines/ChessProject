4151fa766eebeea769787adfcc5dbf1f4edbdb52
mclines <michael.clines@ttu.edu>
2018-01-09 13:22:11 -0600

    Add instructions to getgames() method

--- getInfo.py
+++ getInfo.py
@@ -10,6 +10,12 @@ def extract(data, gameNum):
     game = data["GAME"+str(gameNum)]
     return game
 
+#getGames(end, start, jsonFile) returns a list of nested lists
+# ie. a list of a list of a list.
+# Each index of the return value of getGames() corresponds to a specific chess game.
+# Each game is a list of matrices (list of lists) that cooresponds to sequential moves in the game.
+# the jsonFile and the start index and preinitialized, so unless alternate file or start position required,
+# leave as is. Only required input is end, which is the number of games requested.
 def getGames(end, start = 0, jsonFile = "jsonFiles/jsonTest.json"):
 	data = load(jsonFile)
 	games = [None]*end

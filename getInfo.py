47dfee428c00596676f8f78bb66a7cc9b277fb94
mclines <michael.clines@ttu.edu>
2018-01-10 11:39:33 -0600

    Add method to convert lists to numpy arrays

--- getInfo.py
+++ getInfo.py
@@ -1,3 +1,4 @@
+import numpy
 import json
 
 #used to load the JSON file
@@ -10,10 +11,25 @@ def extract(data, gameNum):
     game = data["GAME"+str(gameNum)]
     return game
 
+# makeArray: list(list(list(int))) -> array(array(array(int)))
+def makeArray(game):
+    #matrices are 64x64 numpy arrays
+    listMatrices = []
+    for move in game:
+        #rows are
+        listRows = []
+        for row in move:
+            r = numpy.array(row)
+            listRows.append(r)
+        matrix = numpy.array(listRows)
+        listMatrices.append(matrix)
+    final = numpy.array(listMatrices)
+    return final
+
 #getGames(end, start, jsonFile) returns a list of nested lists
-# ie. a list of a list of a list.
+# ie. a list of an array of matrices (A matrix is an array of an array)
 # Each index of the return value of getGames() corresponds to a specific chess game.
-# Each game is a list of matrices (list of lists) that cooresponds to sequential moves in the game.
+# Each game is an array of matrices (array of arrays) that cooresponds to sequential moves in the game.
 # The start index is preinitialized, so unless alternate start position required,
 # leave as is. Only required input is number of games (end) and jsonFile, which is the number of games requested and the
 # jsonFile containing desired games.
@@ -22,9 +38,6 @@ def getGames(end, jsonFile, start = 0):
 	games = []
 	for i in range(start,end):
 		game = extract(data,i)
-		#print( len(games) )
-		games.append(game)
+		numGame = makeArray(game)
+		games.append(numGame)
 	return games
-
-#test = getGames(4,'/Users/Mike/Documents/MyCode/jsonFiles/jsonTest.json')
-#print len(test)

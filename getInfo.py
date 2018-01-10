f6468e6acc1fef8d1da49ed00ede1413d123651d
mclines <michael.clines@ttu.edu>
2018-01-10 23:22:53 +0000

    Changed variable input

--- getInfo.py
+++ getInfo.py
@@ -36,17 +36,17 @@ def makeArray(game):
     final = numpy.array(listMatrices)
     return final
 
-#getGames(end, start, jsonFile) returns a list of nested lists
+#getGames(numGames, start, jsonFile) returns a list of nested lists
 # ie. a list of an array of matrices (A matrix is an array of an array)
 # Each index of the return value of getGames() corresponds to a specific chess game.
 # Each game is an array of matrices (array of arrays) that cooresponds to sequential moves in the game.
 # The start index is preinitialized, so unless alternate start position required,
 # leave as is. Only required input is number of games (end) and jsonFile, which is the number of games requested and the
 # jsonFile containing desired games.
-def getGames(end, jsonFile, start = 0):
+def getGames(numGames, jsonFile, start = 0):
 	data = load(jsonFile)
 	games = []
-	for i in range(start,end):
+	for i in range(start,start+numGames):
 		game = extract(data,i)
 		game = changeSize(game)
 		numGame = makeArray(game)
